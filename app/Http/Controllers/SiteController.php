18e64497c4108a6230b868719ffa4f42ebb2349d
Bruno Alleffe <bruno16alleffe@gmail.com>
2023-04-01 20:19:53 -0300

    Adicionada uma filtragem de produtos por categoria

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Produto;
+use App\Models\Categoria;
 
 class SiteController extends Controller
 {
@@ -20,5 +21,12 @@ class SiteController extends Controller
         $produto = Produto::where('slug', $slug)->first();
 
         return view('site.details', compact('produto'));
+    }
+
+    public function categoria($id) {
+        $categoria = Categoria::find($id);
+        $produtos = Produto::where('id_categoria', $id)->paginate(3);
+
+        return view('site.categoria', compact('produtos', 'categoria'));
     }
 }
