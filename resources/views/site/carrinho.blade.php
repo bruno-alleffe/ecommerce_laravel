fca4419395106af3fc2af0a66245a524cc8aa2cb
Bruno Alleffe <bruno16alleffe@gmail.com>
2023-04-05 00:11:20 -0300

    Removendo itens do carrinho

--- resources/views/site/carrinho.blade.php
+++ resources/views/site/carrinho.blade.php
@@ -36,7 +36,11 @@
                     <td><input style="width: 40px; font-weight:900" class="white center" type="number" name="quantity" value="{{$item->quantity}}"></td>
                     <td>
                         <button class="btn-floating waves-effect waves-light orange"><i class="material-icons">refresh</i></button>
-                        <button class="btn-floating waves-effect waves-light red"><i class="material-icons">delete</i></button>
+                        <form action="{{route('site.removeCarrinho')}}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <input type="hidden" name="id" value="{{$item->id}}">
+                            <button class="btn-floating waves-effect waves-light red"><i class="material-icons">delete</i></button>
+                        </form>
                     </td>
                 </tr>
             @endforeach
