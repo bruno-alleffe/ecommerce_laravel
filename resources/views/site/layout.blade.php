5aa4496fe23cea3ef6617d59f6763ed66c361710
Bruno Alleffe <bruno16alleffe@gmail.com>
2023-04-13 18:21:09 -0300

    Adicionado exibição dos dados do usuário logado

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -17,14 +17,25 @@
         @endforeach
     </ul>
 
+    <!-- Dropdown Structure -->
+    <ul id='dropdown2' class='dropdown-content'>
+        <li><a href="{{ route('admin.dashboard')}}">Dashboard</a></li>
+    </ul>
+
     <nav class="red">
         <div class="nav-wrapper container">
             <a href="#" class="brand-logo center">E-commerceLaravel</a>
+            
             <ul style="margin-left: -25px;" id="nav-mobile" class="left">
                 <li><a href="{{ route('site.index')}}">Home</a></li>
                 <li><a href="" class="dropdown-trigger" data-target='dropdown1'>Categorias<i class="material-icons right">expand_more</i> </a></li>
                 <li><a href="{{ route('site.carrinho')}}">Carrinho <span class="new badge blue" data-badge-caption="">{{ \Cart::getContent()->count() }}</span></a></li>
             </ul>
+
+            <ul style="margin-left: -25px;" id="nav-mobile" class="right">
+                <li><a href="" class="dropdown-trigger" data-target='dropdown2'>Olá {{ auth()->user()->firstName }}<i class="material-icons right">expand_more</i> </a></li>
+            </ul>
+        
         </div>
     </nav>
 
