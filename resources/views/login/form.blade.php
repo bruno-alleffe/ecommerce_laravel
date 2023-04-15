a16f96dc2583f143867f52e11a4f4a7817ec8cb0
Bruno Alleffe <bruno16alleffe@gmail.com>
2023-04-15 19:40:55 -0300

    Adicionado função lembrar-me

--- resources/views/login/form.blade.php
+++ resources/views/login/form.blade.php
@@ -15,6 +15,8 @@
     @csrf
     Email: <br><input type="email" name="email"><br>
     Senha: <br><input type="password" name="password"><br>
+    <input type="checkbox" name="remember"> Lembrar-me
+    
     <button type="submit">Entrar</button>
 
 </form>
