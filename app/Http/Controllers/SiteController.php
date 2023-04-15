70fb1cacbc6a78c28a9bf0adcdbe7a84abcd37cd
Bruno Alleffe <bruno16alleffe@gmail.com>
2023-04-15 20:20:53 -0300

    Adicionado autorização com policies

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -20,7 +20,8 @@ class SiteController extends Controller
     public function details($slug) {
         $produto = Produto::where('slug', $slug)->first();
 
-        Gate::authorize('ver-produto', $produto);
+        //Gate::authorize('ver-produto', $produto);
+        $this->authorize('verProduto', $produto);
 
         return view('site.details', compact('produto'));
     }
