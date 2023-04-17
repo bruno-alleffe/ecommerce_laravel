9b0a30cab23a74b71f5ef1d162c2b0065cfe9d7a
Bruno Alleffe <bruno16alleffe@gmail.com>
2023-04-17 19:17:01 -0300

    Adicionado melhorias usando hasMany

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -34,12 +34,12 @@ class DashboardController extends Controller
         $userTotal = implode(',', $total);
 
         //grafico 2 - categorias
-        $catData = Categoria::all();
+        $catData = Categoria::with('produtos')->get();
 
         //preparar array
         foreach($catData as $cat){
             $catNome[] = "'".$cat->nome."'";
-            $catTotal[] = Produto::where('id_categoria', $cat->id)->count();
+            $catTotal[] = $cat->produtos->count();
         }
 
         //formatar para chartjs
