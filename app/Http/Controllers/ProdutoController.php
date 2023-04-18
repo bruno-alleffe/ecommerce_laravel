58d4b49ebba481ae6088765cba465452e4c61b11
Bruno Alleffe <bruno16alleffe@gmail.com>
2023-04-17 22:53:54 -0300

    Adicionado paginação na listagem de produtos

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -14,7 +14,7 @@ class ProdutoController extends Controller
      */
     public function index()
     {
-        $produtos = Produto::all();
+        $produtos = Produto::paginate(5);
 
         return view('admin.produtos', compact('produtos'));
     }
